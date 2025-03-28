c047b9fec98328556b5e73016b7cbec399a6a463
Sanjeev Kumar Singh <iamsanjeevkrsingh@gmail.com>
2025-03-28 21:13:58 +0530

    learned prototype

--- prototype.js
+++ prototype.js
@@ -72,7 +72,6 @@
 // }
 
 // in the above approach we are repeating the same code for each student
-
 // 3rd approach use constructor function
 
 // function CreateStudent(name, age) {
@@ -103,7 +102,148 @@
 // let student3 = new CreateStudent("Jim", 22);
 // console.log(student3);
 
-// suppose we create 100 students then 100 times the function will be created
-// memory will be wasted
+// suppose we create 100 students then 100 times the function will be created memory will be wasted
 
-// 4th approach create one function and use it for all students 
+// 4th approach create one function and use it for all students
+
+// function MyObjectCreate(obj) {
+//     function F() {}
+//     F.prototype = obj;
+//     return new F();
+// }
+
+// function CreateStudent(name, age) {
+//     const obj = Object.create(studentTask);
+//     obj.name = name;
+//     obj.age = age;
+//     return obj;
+// }
+
+
+// const studentTask = {
+//     checkEligibleForNextClass: function() {
+//         if (this.age > 18) {
+//             console.log("Eligible for next class", this);
+//         }
+//     },
+//     // isValidName: function() {
+//     //     if (this.name.length > 3) {
+//     //         console.log("Valid name", this);
+//     //     } else {
+//     //         console.log("Invalid name", this);
+//     //     }
+//     // }
+// }
+
+// const student1 = CreateStudent("John", 20);
+// const student2 = CreateStudent("Jane", 21);
+// const student3 = CreateStudent("Jim", 22);
+
+// console.log(student1);
+// student1.checkEligibleForNextClass();
+// // student1.isValidName();
+
+// student2.checkEligibleForNextClass();
+// // student2.isValidName();
+
+// student3.checkEligibleForNextClass();
+// // student3.isValidName();
+
+
+function MakeStudent(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+MakeStudent.prototype.checkEligibleForNextClass = function() {
+    if (this.age > 18) {
+        console.log("Eligible for next class", this);
+    }else{
+        console.log("Not eligible for next class", this);
+    }
+}
+
+const student1 = new MakeStudent("John", 20);
+console.log(student1);
+student1.checkEligibleForNextClass();
+const fn = student1.checkEligibleForNextClass;
+fn();
+
+
+
+
+
+
+
+
+
+
+
+// function parent(name) {
+//     this.name = name;
+// }
+
+// const parent1 = new parent("John");
+// console.log(Object.getPrototypeOf(parent1) === parent.prototype);
+
+// function child(name) {
+//     this.name = name;
+// }
+
+// child.prototype = parent;
+
+// const child1 = new child("Jane");
+// console.log(child1);
+
+
+// Default behavior of function
+
+// function show() {
+//     console.log("show");
+// }
+
+// console.dir(show);
+
+// 1. Every function has one special property called prototype
+// 2. Prototype is an object
+// 3. Prototype is used to store methods and properties that are shared by all instances of the function
+// 4. Prototype is used to implement inheritance
+
+
+// 1. Every object has one special property called __proto__ or Object.getPrototypeOf()
+// 2. __proto__ is an object
+// 3. __proto__ is used to store methods and properties that are shared by all instances of the object
+// 4. __proto__ is used to implement inheritance
+
+
+// function parent() {
+
+// }
+
+// function child(name) {
+//     this.name = name;
+// }
+
+// child.prototype = parent;
+
+// const child1 = new child("John");
+// console.log(child1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
